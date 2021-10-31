f74d524d7db8ef7b1cee9a2f4d047d9c9d9ad87a
nataworn13 <nut_nataworn@hotmail.com>
2021-10-31 23:30:35 +0700

    final

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-contactus',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactusComponent implements OnInit {
 
+    getName(){
+      Swal.fire(
+        'Success!',
+        'ส่งรายละเอียดเสร็จสิ้น!<br>ขอบคุณสำหรับการติดต่อ',
+        'success'
+      )
+    }
+    
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
 }
